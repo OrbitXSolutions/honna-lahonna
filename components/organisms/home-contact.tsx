76c772a6e8c345e1d8c66060b1562fcf411e8ca6
Abdallah Elrashedy <abodyrody55@gmail.com>
2025-06-07 02:08:16 +0400

    fixes

--- components/organisms/home-contact.tsx
+++ components/organisms/home-contact.tsx
@@ -1,6 +1,10 @@
 import { HTMLProps } from "react";
 import GridBackground from "../molecules/grid-background";
 import Image from "next/image";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import AppButton from "../atoms/app-button";
+import { IconArrow } from "../icons";
 const ContactImage = ({ className, ...params }: HTMLProps<any>) => {
   return (
     <div
@@ -36,11 +40,53 @@ const ContactImage = ({ className, ...params }: HTMLProps<any>) => {
   );
 };
 
+const ContactForm = () => {
+  return (
+    <div className="mx-auto space-y-6">
+      <div className="text-center space-y-4">
+        <p>
+          <span className="font-medium">{"ــــــــــــــ"}</span>&nbsp;
+          <span className="font-medium">{"تواصل معنا"}</span>
+        </p>
+
+        <h2 className="text-3xl md:text-4xl font-bold">
+          <span>{"نرحب بتواصلك"}</span>
+        </h2>
+      </div>
+      <form className="space-y-4 w-full">
+        <Input type="text" placeholder="الاسم" className="w-full bg-white" />
+        <Input
+          type="email"
+          placeholder="البريد الإلكتروني"
+          className="w-full bg-white"
+        />
+        <Input
+          type="tel"
+          placeholder="رقم الهاتف"
+          className="w-full bg-white"
+        />
+        <Textarea
+          placeholder="اكتبي رسالتك هنا ..."
+          className="w-full bg-white min-h-32"
+        />
+        <AppButton type="submit" className="text-center w-full">
+          {"إرسال"}
+
+          <IconArrow />
+        </AppButton>
+      </form>
+    </div>
+  );
+};
+
 export default function HomeContact() {
   return (
     <section className="contactrelative">
-      <GridBackground color="text-background" className="space-y-10">
-        <ContactImage />
+      <GridBackground color="text-background" className="">
+        <div className="container flex mx-auto gap-20">
+          <ContactImage />
+          <ContactForm />
+        </div>
       </GridBackground>
     </section>
   );
