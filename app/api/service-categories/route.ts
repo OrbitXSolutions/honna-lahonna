96e17e6d5772cb713ade550a507468029ae62729
Abdallah Elrashedy <abodyrody55@gmail.com>
2025-06-07 02:28:15 +0400

    .

--- app/api/service-categories/route.ts
+++ app/api/service-categories/route.ts
@@ -1,7 +1,6 @@
 // app/api/services/route.ts
 import { NextResponse } from "next/server";
 import { getServiceCategories } from "@/lib/data/service-categories";
-export const revalidate = 60 * 60; // Revalidate every hour
 
 export async function GET(request: Request) {
   const serviceCategories = await getServiceCategories();
