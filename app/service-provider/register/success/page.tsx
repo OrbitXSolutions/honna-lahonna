c04e167ad6bfd81b8c69c16952a1f83c75bb86ee
Abdallah Elrashedy <abodyrody55@gmail.com>
2025-06-12 02:31:25 +0400

    fixes 2

---
+++ app/service-provider/register/success/page.tsx
@@ -0,0 +1,8 @@
+export default function ServiceProviderRegisterPage() {
+  return (
+    <div>
+      <h1>Registration Successful</h1>
+      <p>Your service provider account has been successfully registered.</p>
+    </div>
+  );
+}
