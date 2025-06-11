f747a8492d5b9a9d9126df6374b14fed7475fbaf
Abdallah Elrashedy <abodyrody55@gmail.com>
2025-06-12 02:29:38 +0400

    fixes

--- app/api/service-categories/route.ts
+++ app/api/service-categories/route.ts
@@ -1,6 +1,6 @@
 // app/api/services/route.ts
+import { getServiceCategories } from "@/lib/data/prisma/service-categories";
 import { NextResponse } from "next/server";
-import { getServiceCategories } from "@/lib/data/service-categories";
 
 export async function GET(request: Request) {
   const serviceCategories = await getServiceCategories();
