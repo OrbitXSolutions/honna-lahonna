6f3d75ffdabe89602f1e3e484de5ac64e572806f
Abdallah Elrashedy <abodyrody55@gmail.com>
2025-06-15 17:05:57 +0400

    fixes

--- app/api/governorates/list/route.ts
+++ app/api/governorates/list/route.ts
@@ -2,7 +2,7 @@ import { getGovernorates } from "@/lib/data/prisma/governorates";
 export const dynamic = "force-static";
 export const revalidate = 60;
 
-export async function GET(request: Request, response: Response) {
+export async function GET() {
   const governorates = await getGovernorates();
 
   return new Response(JSON.stringify(governorates), {
